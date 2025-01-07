37e1ca155c94a8b94583917a055ee5626a1ab26b
Mr Esbatu <qorri24j@gmail.com>
2025-01-07 14:46:42 +0700

    ExampleKafkaApplication

---
+++ src/main/java/com/qorri/kafka/ExampleKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.qorri.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleKafkaApplication.class, args);
+	}
+
+}
