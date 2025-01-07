c96326595e910bf01cbb397bb87994ec03e44152
Mr Esbatu <qorri24j@gmail.com>
2025-01-07 14:46:57 +0700

    KafkaRequest

---
+++ src/main/java/com/qorri/kafka/dto/KafkaRequest.java
@@ -0,0 +1,13 @@
+package com.qorri.kafka.dto;
+
+public class KafkaRequest {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
